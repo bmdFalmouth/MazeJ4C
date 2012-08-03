d00015828a79e30ee236b5abd36a091b5a7b964a
Matthew Mitchell <mancer@live.com>
2012-08-03 18:50:27 +0100

    initial GUI script

---
+++ Assets/Game/Scripts/GameGUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameGUI : MonoBehaviour {
+	
+	public int score = 0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
