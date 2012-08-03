63632d3b9814ac6cfb40e26ea81a68d391c08d75
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2012-08-03 21:45:32 +0100

    Added arrow display

--- Assets/Game/Scripts/GameGUI.cs
+++ Assets/Game/Scripts/GameGUI.cs
@@ -5,13 +5,66 @@ public class GameGUI : MonoBehaviour {
 	
 	public int score = 0;
 	
+	public GameObject upArrow;
+	public GameObject downArrow;
+	public GameObject leftArrow;
+	public GameObject rightArrow;
+	
+	private GameObject firedObject;
+	
+	public float timeStart=0.0f;
+	public bool startTimer=false;
+	
 	// Use this for initialization
 	void Start () {
-	
+		upArrow.active=false;
+		downArrow.active=false;
+		leftArrow.active=false;
+		rightArrow.active=false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown("1"))
+		{
+			if (!startTimer){
+				startTimer=true;
+			}
+			firedObject=upArrow;
+		}
+		if (Input.GetKeyDown("2"))
+		{
+			if (!startTimer){
+				startTimer=true;
+			}
+			firedObject=downArrow;			
+		}
+		if (Input.GetKeyDown("3"))
+		{
+			if (!startTimer){
+				startTimer=true;
+			}
+			firedObject=leftArrow;			
+		}
+		if (Input.GetKeyDown("4"))
+		{
+			if (!startTimer){
+				startTimer=true;
+			}
+			firedObject=rightArrow;		
+			
+		}
+		
+		if (startTimer)
+		{
+			firedObject.active=true;
+			timeStart+=Time.deltaTime;
+			if (timeStart>2.0f)
+			{
+				startTimer=false;
+				firedObject.active=false;
+				timeStart=0.0f;
+			}
+		}
 	}
 }
