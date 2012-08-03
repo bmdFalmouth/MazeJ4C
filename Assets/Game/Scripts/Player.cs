396513e1b3249467f65b0eb4dd0a8f1df9e011a5
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2012-08-03 18:45:06 +0100

    Initial First script

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float speed=10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
