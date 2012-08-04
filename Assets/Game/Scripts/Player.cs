d05e26045b3202db4344eaaa019260e38a29007c
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2012-08-04 10:01:03 +0100

    Added changes

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-using UnityEngine;
-using System.Collections;
-
-public class Player : MonoBehaviour {
-	
-	public float speed=10.0f;
-	public Transform spotLightTransform;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//have to play the character audio here!!!
-		
-		rigidbody.AddTorque(-speed*Input.GetAxis("Horizontal")*Vector3.forward);
-		rigidbody.AddTorque(speed*Input.GetAxis("Vertical")*Vector3.right);
-		
-		Vector3 spotPosUpdate=spotLightTransform.position;
-		spotPosUpdate.x=transform.position.x;
-		spotPosUpdate.z=transform.position.z;
-		spotLightTransform.position=spotPosUpdate;
-	}
-}
-=======
 using UnityEngine;
 using System.Collections;
 
@@ -71,4 +43,3 @@ public class Player : MonoBehaviour
 		
 	}
 }
->>>>>>> 97c767be637fe46e08f0bef1e0bac8c8dc9e6419
