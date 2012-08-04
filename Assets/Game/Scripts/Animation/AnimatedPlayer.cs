98dcab331a45f4228de001c29d7fbcf0f13a376c
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2012-08-04 13:29:08 +0100

    Initial Animation States

--- Assets/Game/Scripts/Animation/AnimatedPlayer.cs
+++ Assets/Game/Scripts/Animation/AnimatedPlayer.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class AnimatedPlayer : MonoBehaviour {
 	
-	public OTAnimatingSprite gun;              // gun sprite reference
+	public enum AnimationState
+	{
+		idleFront=0,
+		idleBack,
+		idleLeft,
+		idleRight
+	};
+	
+	public OTAnimatingSprite animatedSprite;              // gun sprite reference
+	public AnimationState animationState=AnimationState.idleFront;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -11,13 +21,50 @@ public class AnimatedPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1"))
+		if (Input.GetAxis("Horizontal")>0)
 		{
-			gun.PlayOnce("shoot");
+			animationState=AnimationState.idleRight;
 		}
-		else
+		else if (Input.GetAxis("Horizontal")<0)
 		{
-			gun.PlayLoop("idle");
+			animationState=AnimationState.idleLeft;
 		}
+		else if (Input.GetAxis("Vertical")>0)
+		{
+			animationState=AnimationState.idleBack;
+		}
+		else if (Input.GetAxis("Vertical")<0)
+		{
+			animationState=AnimationState.idleFront;
+		}		
+		UpdateAnimation();
+	}
+	
+	void UpdateAnimation()
+	{
+		
+		switch(animationState)
+		{
+			case AnimationState.idleFront:
+			{
+				animatedSprite.Play("idleFront");
+				break;
+			}
+			case AnimationState.idleBack:
+			{
+				animatedSprite.Play("idleBack");
+				break;
+			}
+			case AnimationState.idleLeft:
+			{
+				animatedSprite.Play("idleLeft");
+				break;
+			}			
+			case AnimationState.idleRight:
+			{
+				animatedSprite.Play("idleRight");
+				break;
+			}			
+		}		
 	}
 }
